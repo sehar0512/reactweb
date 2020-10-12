f89734b8c05a51813698930964caeef0cd0f0a3e
sehar fatima <sehar0234@gmail.com>
2020-10-12 20:10:25 +0500

    Updates

---
+++ precache-manifest.6cfc65519a1c4c8be70c63c1317fa347.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9732629f58809489a49c7c4f1924ae3",
+    "url": "/reactwebsite/index.html"
+  },
+  {
+    "revision": "b51a3e8d9110428c8bc8",
+    "url": "/reactwebsite/static/css/2.72c58b48.chunk.css"
+  },
+  {
+    "revision": "a9fee499fb35f221d8c2",
+    "url": "/reactwebsite/static/css/main.59986262.chunk.css"
+  },
+  {
+    "revision": "b51a3e8d9110428c8bc8",
+    "url": "/reactwebsite/static/js/2.104be708.chunk.js"
+  },
+  {
+    "revision": "d305702e104edbbcfe5e76fbd386fffe",
+    "url": "/reactwebsite/static/js/2.104be708.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9fee499fb35f221d8c2",
+    "url": "/reactwebsite/static/js/main.8f123c07.chunk.js"
+  },
+  {
+    "revision": "548f392c67800bc84ffd",
+    "url": "/reactwebsite/static/js/runtime-main.7f1b8b40.js"
+  },
+  {
+    "revision": "445cb64b00fa1c0e28135bad6327114e",
+    "url": "/reactwebsite/static/media/aboutBg.445cb64b.jpg"
+  },
+  {
+    "revision": "641d32f04bcce1fa159b596b25c8d459",
+    "url": "/reactwebsite/static/media/bg.641d32f0.jpg"
+  },
+  {
+    "revision": "29d139720d5057a1f7f3b448a6104180",
+    "url": "/reactwebsite/static/media/brush.29d13972.jpg"
+  },
+  {
+    "revision": "40ca25ccda3aee3925761f88a486df1e",
+    "url": "/reactwebsite/static/media/cream.40ca25cc.jpg"
+  },
+  {
+    "revision": "48f22a641e4c5e0f8ba7ba3401f7d6ed",
+    "url": "/reactwebsite/static/media/eyeshade.48f22a64.jpg"
+  },
+  {
+    "revision": "2ad9bdac61f36ef24439162e10854054",
+    "url": "/reactwebsite/static/media/glimmer.2ad9bdac.jpg"
+  },
+  {
+    "revision": "ec7d73c1748477406773ed5322d5eb23",
+    "url": "/reactwebsite/static/media/nailpolish.ec7d73c1.jpg"
+  },
+  {
+    "revision": "b776fcdefdc55eebb2187c482546c37b",
+    "url": "/reactwebsite/static/media/perfume.b776fcde.jpg"
+  }
+]);
